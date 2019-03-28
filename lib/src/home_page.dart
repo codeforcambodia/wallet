35abd0af09d95a4e853a8220db1570f74d8b9689
DaveatCor <condaveat@gmail.com>
2019-03-28 13:15:42 +0700

    done

--- lib/src/home_page.dart
+++ lib/src/home_page.dart
@@ -1,15 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:flutter_auth_buttons/flutter_auth_buttons.dart';
+import 'dart:async';
+import 'package:http/http.dart' as http;
 
-class home_page extends StatelessWidget{
+GoogleSignIn _googleSignIn =GoogleSignIn(
+  scopes: <String>[
+    'email',
+    'https://www.googleapis.com/auth/contacts/readonly',
+  ]
+);
 
-  // GoogleSignIn _googleSignIn = new GoogleSignIn(
-  //   scopes: [
-  //     'email',
-  //     'https://www.googleapis.com/auth/contacts.readonly'
-  //   ]
-  // );
+class home_page extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return homepagestate();
+  }
+}
 
+class homepagestate extends State<home_page>{
+
+  GoogleSignInAccount _currentUser;
+
+  String _contactText;  
+
+  void initState() {
+    super.initState();
+    _googleSignIn.onCurrentUserChanged.listen((GoogleSignInAccount account) {
+      setState(() {
+        _currentUser =account;
+      });
+      if (_currentUser != null) {
+        google_sign_in();
+      }
+    });
+  }
+
+  Future<void> google_sign_in() async {
+    await _googleSignIn.signIn();
+  }
+  
   Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(
@@ -22,8 +52,9 @@ class home_page extends StatelessWidget{
   }
 
   Widget googleSignUp() {
+    return GoogleSignInButton(
+      onPressed: () { google_sign_in(); },
+    );
   }
-  // Future<bool> loginWithGoogle() async{
-  //   final api = awit 
-  // } 
+
 }
