2b9c29767fab73b850fad459ff3c3edf890c37f9
DaveatCor <condaveat@gmail.com>
2019-04-09 09:01:06 +0700

    done on android

--- lib/src/home_page.dart
+++ lib/src/home_page.dart
@@ -2,12 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter_auth_buttons/flutter_auth_buttons.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:circular_profile_avatar/circular_profile_avatar.dart';
-// import 'package:flutter_facebook_login/flutter_facebook_login.dart';
+import 'package:flutter_facebook_login/flutter_facebook_login.dart';
+import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
 
 GoogleSignIn _googleSignIn = GoogleSignIn(scopes: <String>[
   'email',
   'https://www.googleapis.com/auth/contacts.readonly'
 ]);
+
+FacebookLogin facebookLogin = new FacebookLogin();
 
 class home_page extends StatefulWidget {
   @override
@@ -30,6 +34,28 @@ class homepageState extends State<home_page> {
     });
   }
 
+  void initiateFacebookLogin() async {
+    final result = await facebookLogin.logInWithReadPermissions(['email', 'public_profile']);
+    final token = result.accessToken.token;
+    final graphResponse = await http.get(
+      'https://graph.facebook.com/v2.12/me?fields=birthday,email&access_token=$token');  
+    print(graphResponse.body);
+    switch (result.status) {
+      case FacebookLoginStatus.error:
+        print("Error");
+        // onLoginStatusChanged(false);
+        break;
+      case FacebookLoginStatus.cancelledByUser:
+        print("CancelledByUser");
+        // onLoginStatusChanged(false);
+        break;
+      case FacebookLoginStatus.loggedIn:
+        print("LoggedIn");
+        // onLoginStatusChanged(true);
+        break;
+    }  
+  }
+
   Widget build(BuildContext context) {
     return DefaultTabController(
       length: 4,
@@ -43,31 +69,39 @@ class homepageState extends State<home_page> {
           ),
           body: Center(
             child: _account == null
-                ? googleButton()
-                : Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Container(
-                        margin: EdgeInsets.only(top: 20.0),
-                      ),
-                      CircularProfileAvatar(
-                        _account.photoUrl,
-                        radius: 50.0,
-                        borderWidth: 5,
-                        borderColor: Colors.white,
-                      ),
-                      Container(margin: EdgeInsets.only(left: 20.0)),
-                      Column(
-                        children: <Widget>[
-                          Text(_account.displayName),
-                          Text(_account.email)
-                        ],
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                      ),
-                      googleButton()
-                    ],
-                  ),
+                ? 
+                // googleButton()
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget> [
+                    facebookButton(),
+                    googleButton()
+                  ],
+                )
+                : Row (
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.only(top: 20.0),
+                    ),
+                    CircularProfileAvatar(
+                      _account.photoUrl,
+                      radius: 50.0,
+                      borderWidth: 5,
+                      borderColor: Colors.white,
+                    ),
+                    Container(margin: EdgeInsets.only(left: 20.0)),
+                    Column(
+                      children: <Widget>[
+                        Text(_account.displayName),
+                        Text(_account.email)
+                      ],
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                    ),
+                    googleButton()
+                  ],
+                ),
           ),
           bottomNavigationBar: TabBar(
             tabs: <Widget>[
@@ -105,9 +139,23 @@ class homepageState extends State<home_page> {
       return IconButton(
         icon: Icon(Icons.power_settings_new),
         iconSize: 28.0,
-        onPressed: () => googleSignOut(),
+        onPressed: () {
+          googleSignOut();
+        },
       );
     }
+  }
+
+  Widget facebookButton() {
+    return FacebookSignInButton(
+      onPressed: () {
+        initiateFacebookLogin();
+      },
+    );
+  }
+
+  Widget circularProgress() {
+    return CircularProgressIndicator();
   }
 
   void googleSignUp() {
@@ -117,5 +165,10 @@ class homepageState extends State<home_page> {
 
   void googleSignOut() {
     _googleSignIn.signOut();
+  }
+
+  Future<void> facebookSignOut() async{
+    var channel = MethodChannel('com.roughike/flutter_facebook_login');
+    channel.invokeMethod('logOut');
   }
 }
